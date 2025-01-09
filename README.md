bf676c99f8d4d440d264e17d1ec4e7bb78a03042
Anwar Ahmed <anwar.ahmed@gmail.com>
2025-01-09 00:03:53 -0500

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# wezterm
-My Wezterm setup
+# Wezterm
+
+This is my personal setup for [Wezterm](https://wezfurlong.org/wezterm/), a GPU-accelerated terminal emulator and multiplexer.
+
+Copy the contents of the repository to `~/.config/wezterm/` and restart Wezterm.
+The following files can be excluded:
+
+- `README.md`
+- `.gitignore`
+- `.git`
---
+++ wezterm.lua
@@ -0,0 +1,24 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = "Grape"
+
+-- Font:
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 14.0
+
+-- Window transparency
+config.window_background_opacity = 0.9
+config.macos_window_background_blur = 15
+
+-- Tabs
+config.hide_tab_bar_if_only_one_tab = true
+
+-- and finally, return the configuration to wezterm
+return config
