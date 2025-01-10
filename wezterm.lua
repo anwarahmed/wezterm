a216dd93297c6edd2debbf1e5ca034bf00280a05
Anwar Ahmed <anwar.ahmed@gmail.com>
2025-01-10 17:01:01 -0500

    changed theme to catppuccin mocha (#2)

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Grape"
+config.color_scheme = "Catppuccin Mocha"
 
 -- Font:
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
