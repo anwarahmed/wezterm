fec9dbfea689f82f7328ddbf12bbbc621083a664
Anwar Ahmed <anwar.ahmed@gmail.com>
2025-01-15 17:34:27 -0500

    updated theme to github dark (#3)

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "GitHub Dark"
 
 -- Font:
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
